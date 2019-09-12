7d9c3204122bfcff1923e3fda83287a483f3a032
Paul Tanchareon <paulvtan@live.com>
2019-09-12 14:59:24 +1200

    Removed reality-check folder

--- reality-check/src/Pages/heros/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Heros = ({children}) => {
-    return <div>Heros page</div>
-}
-
-export default Heros
--- reality-check/src/Pages/trash-talk/trashTalkLocal.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const TrashTalkLocal = ({children}) => {
-    return <div>Trash Talk</div>
-}
-
-export default TrashTalkLocal
